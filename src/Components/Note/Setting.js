d3466114b4e940d131b7e3c143dd390a82cfa080
Yuta Mori <5569823+YutaMoriJP@users.noreply.github.com>
2021-04-06 01:36:05 -0700

    notifaction appears at bottom left after setting is changed

--- src/Components/Note/Setting.js
+++ src/Components/Note/Setting.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import SettingContainer from "./styled/SettingContainer";
 import styled from "styled-components";
 import Button from "@material-ui/core/Button";
 import VerticalAlignBottomIcon from "@material-ui/icons/VerticalAlignBottom";
 import VerticalAlignTopIcon from "@material-ui/icons/VerticalAlignTop";
+import Notifaction from "./Notification";
 
 const P = styled.p`
   width: 20px;
@@ -13,36 +14,56 @@ const P = styled.p`
 `;
 
 const Setting = ({ setColor, setOrder, order }) => {
-  const pickGreen = () => setColor("#d3f9d8");
-  const pickYellow = () => setColor("#ffec99");
-  const pickPurple = () => setColor("#e5dbff");
+  const [open, setOpen] = useState(false);
+  const [msg, setMsg] = useState("Setting is saved");
+  const handleNotifaciton = () => setOpen(true);
+  const pickGreen = () => {
+    setColor("#d3f9d8");
+    handleNotifaciton();
+    setMsg("Background was set to green");
+  };
+  const pickYellow = () => {
+    setColor("#ffec99");
+    handleNotifaciton();
+    setMsg("Background was set to yellow.");
+  };
+  const pickPurple = () => {
+    setColor("#e5dbff");
+    handleNotifaciton();
+    setMsg("Background was set to purple");
+  };
   const pickOrder = event => {
     setOrder(event.target.innerText.toLowerCase());
+    handleNotifaciton();
+    setMsg(`Posts are sorted from ${event.target.innerText.toLowerCase()}`);
   };
   return (
-    <SettingContainer>
-      <article>
-        <p>Note Background</p>
-        <section>
-          <P color="#d3f9d8" onClick={pickGreen}></P>
-          <P color="#e5dbff" onClick={pickPurple}></P>
-          <P color="#ffec99" onClick={pickYellow}></P>
-        </section>
-      </article>
-      <article>
-        <p>Sort by</p>
-        <section>
-          <Button onClick={pickOrder}>
-            {order === "top" ? "bottom" : "top"}
-          </Button>
-          {order === "top" ? (
-            <VerticalAlignBottomIcon />
-          ) : (
-            <VerticalAlignTopIcon />
-          )}
-        </section>
-      </article>
-    </SettingContainer>
+    <>
+      <SettingContainer>
+        <article>
+          <p>Note Background</p>
+          <section>
+            <P color="#d3f9d8" onClick={pickGreen}></P>
+            <P color="#e5dbff" onClick={pickPurple}></P>
+            <P color="#ffec99" onClick={pickYellow}></P>
+          </section>
+        </article>
+        <article>
+          <p>Sort by</p>
+          <section>
+            <Button onClick={pickOrder}>
+              {order === "top" ? "bottom" : "top"}
+            </Button>
+            {order === "top" ? (
+              <VerticalAlignBottomIcon />
+            ) : (
+              <VerticalAlignTopIcon />
+            )}
+          </section>
+        </article>
+      </SettingContainer>
+      <Notifaction open={open} setOpen={setOpen} msg={msg} />
+    </>
   );
 };
 
